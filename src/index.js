6ac5b13f2c2a41b5ec89f309195ffcf62fa3721e
An Pham <53116737+anpham148@users.noreply.github.com>
2021-04-26 13:53:47 +0100

    Created combine reducers for plants + collections + users and actions for fetching plants and testing actions fetching (#2)
    * added combine reducers for plant + collection + user
    
    * create actions + testing fetch in actions

--- src/index.js
+++ src/index.js
@@ -4,10 +4,11 @@ import App from './App';
 import { createStore , applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import rootReducer from './reducers/manageUsersCollectionsPlants';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+let store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ src/reducers/manageUsersCollectionsPlants.js
@@ -0,0 +1,47 @@
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+    users: usersReducer,
+    collections: collectionsReducer,
+    plants: plantsReducer
+});
+
+export default rootReducer;
+
+function usersReducer(state = [], action) {
+    switch(action.type) {
+        case "ADD_USER":
+            return [...state, action.user];
+        default:
+            return state;
+    }
+}
+
+function collectionsReducer(state = [], action) {
+   let idx;
+    switch(action.type) {
+        case "ADD_COLLECTION":
+            return [...state, action.collection];
+
+        case "REMOVE_COLLECTION":
+            idx = state.findIndex(collection => collection.id === action.id)
+            return [...state.slice(0, idx), ...state.slice(idx+1)];
+
+        default:
+            return state;
+    }
+}
+
+function plantsReducer(state = [], action) {
+    switch(action.type) {
+        case "FETCH_PLANTS":
+            return [...state];
+
+        case "ADD_PLANT":
+            return [...state, action.plant];
+
+        default:
+            return state;
+    }
+
+}
