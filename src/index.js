03072156089864252f1c87e25c9147e758590d2f
anpham148 <anpham148@gmail.com>
2021-04-26 14:48:45 +0100

    installed the Auth0 React SDK

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
 import { createStore , applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
@@ -11,8 +12,16 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 let store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <Auth0Provider
+    domain="an-tp.eu.auth0.com"
+    clientId="ODD637c2s38AAswbXggHMg8CEVVbl0IQ"
+    redirectUri={window.location.origin}
+  >
+    <Provider store={store}>
+      <App />
+    </Provider>
+    
+  </Auth0Provider>
+  ,
   document.getElementById('root')
 );
