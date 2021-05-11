3ece90af532c04967dcb4321dbb30f2a222d31bf
anpham148 <anpham148@gmail.com>
2021-05-11 11:41:04 +0100

    added formatted_name for plant

--- src/plants/Plant.js
+++ src/plants/Plant.js
@@ -9,7 +9,7 @@ const Plant = ( {plant} ) => {
     return (
         <div>
                  <img src={plant.attributes.image_url} width="600" alt={plant.attributes.name}/> <br />
-                 1. Name: {plant.attributes.name} (aka: {plant.attributes.aka})<br />
+                 1. Name: {plant.attributes.formatted_name} (aka: {plant.attributes.aka})<br />
                  2. Description: {plant.attributes.description}<br />
                  3. Recomended pot size: {plant.attributes.size_pot} inch<br />
                  4. Pet friendly: {plant.attributes.pet_friendly ?  "Yes" : "No"} <br /> <br />
--- src/plants/Plants.js
+++ src/plants/Plants.js
@@ -6,11 +6,11 @@ const Plants = ( {plants} ) => {
     return (
         <div>
             <ul>
-                {plants.map(plant => (
+                {plants.map(plant => plant && (
                     <Link to={`/plants/${plant.attributes.slug}`} key={plant.id}>
                         <li>
                             <img src={plant.attributes.image_url} width="300" alt={plant.attributes.name}/> <br />
-                            {plant.attributes.name}
+                            {plant.attributes.formatted_name}
                         </li>
                     </Link>))
                 } 
