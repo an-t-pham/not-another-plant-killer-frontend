c2d37aecf9e29ce141f2166b41edd5d6d1e67629
anpham148 <anpham148@gmail.com>
2021-05-12 16:31:56 +0100

    added containers folder for collection

--- src/routers/CollectionsRouter.js
+++ src/routers/CollectionsRouter.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import CollectionInput from '../collection/CollectionInput';
-import Collections from '../collection/Collections';
+import { Route, Switch } from 'react-router-dom';
+import CollectionInput from '../collections/CollectionInput';
+import Collections from '../collections/Collections';
 
 export default class CollectionsContainer extends React.Component {
     render() {
         return (
            <div>
+               <Switch>
+                 <Route exact path="/plants" render={() =><PlantsContainer plants={this.props.plants} /> } />
+                 <Route path="/plants/:slug"  render={(routerProps) =><PlantContainer {...routerProps}/> }/>
+                 <Route path="/plants/your-garden" render={() => <UserPlants/> }/>
+               </Switch>
                <CollectionInput />
                <Collections />
            </div>
