beb0fd4727f7228faba222e9060a11e308d9625c
An Pham <anpham148@gmail.com>
2021-07-08 13:11:30 +0100

    CollectionContainer (#42)
    * edited CollectionContainer to functional component
    
    * deleted valur in modal + edited functions
    
    * cleanup CollectionContainer
    
    * added dependencies in useEffect in CollectionContainer

--- src/collections/containers/CollectionContainer.js
+++ src/collections/containers/CollectionContainer.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Collection from '../Collection';
 import CollectionInput  from '../CollectionInput';
 
@@ -14,93 +14,74 @@ import Modal from '@material-ui/core/Modal';
 import FabButton from '../../components/FabButton';
 
 
+const CollectionContainer = ({ match, history }) => {
+    const dispatch = useDispatch();
 
-class CollectionContainer extends React.Component {
+    const collections = useSelector((state) => state.collections);    
+    const user = useSelector((state) => state.user);  
+    const plants = useSelector((state) => state.plants);  
 
-     componentDidUpdate() {
-       if (this.props.user && this.props.collections.length === 0) {
-         return this.props.fetchCollections(this.props.user.id)
-       } 
+  useEffect(() => {
+     if (user && collections.length === 0) {
+       return dispatch(fetchCollections(user.id))
      }
+  },[user, collections.length, dispatch])
 
-    
-    state = {
-        showEditForm: false
-    }
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
 
-    handleOpen = () => {
-      this.setState({
-        showEditForm: true
-      })
-   }
-  
-     handleClose = () => {
-        this.setState({
-          showEditForm: false
-        })
-     }
+  const handleOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
 
-    findCollection = () => {
-      let collectionSlug = this.props.match && (this.props.match.params.slug);
-      let collection = this.props.collections && (this.props.collections.find(collection => collection.attributes.slug === collectionSlug)) ;
-      return collection
-    }
+  const collection = useMemo(() => {
+    let collectionSlug = match && (match.params.slug);
+    let collection = collections && (collections.find(collection => collection.attributes.slug === collectionSlug)) ;
+    return collection
+  }, [collections, match])
+ 
 
-     handleSubmit = (collectionData) => {
-        const collection = this.findCollection();
-        this.props.user && (this.props.editCollection(this.props.user.id, collection.id, collectionData));
-        this.props.history.push("/profile/collections");
-        this.handleClose();
-    }
-
-     deleteCollection = () => {
-        const collection = this.findCollection();
-        this.props.deleteCollection(this.props.user.id, collection.id);
-        this.props.history.push("/profile/collections");
-     }
-
-     deletePlantfromCollection = (plantData) => {
-       const collection = this.findCollection();
-       this.props.user && (this.props.deletePlantfromCollection(this.props.user.id, collection.id, plantData));
-     }
-
-  render() {
-     const collection = this.findCollection();
-
-    return (
-      
-       <div>
-            <Collection collection={collection} plants={this.props.plants} deletePlantfromCollection={this.deletePlantfromCollection}/>
-            <div style={{position: 'fixed', top: '120px', right: '20px'}}>
-              <FabButton title="Edit Collection" button="edit" handleAction={this.handleOpen} right="40px" />
-            </div>
-            
-            <Modal
-              open={this.state.showEditForm}
-              onClose={this.handleClose}
-              value={this.state.name}
-            > 
-            <CollectionInput collection={collection}  handleSubmit={this.handleSubmit} /> 
-            </Modal>
-
-            <div style={{position: 'fixed', top: '190px', right: '20px'}}>
-               <FabButton title="Delete Collection" button="delete" handleAction={this.deleteCollection} right="50px" />
-            </div>
-           
-       </div>
-    )
+  const handleSubmit = (collectionData) => {
+    user && dispatch(editCollection(user.id, collection.id, collectionData));
+    history.push("/profile/collections");
+    handleClose();
   }
 
+  const deleteTheCollection = () => {
+    user && dispatch(deleteCollection(user.id, collection.id));
+    history.push("/profile/collections");
+  }
+
+  const deleteThePlantfromCollection = (plantData) => {
+    user && dispatch(deletePlantfromCollection(user.id, collection.id, plantData));
+  }
+
+ 
+  return (   
+    <div>
+        <Collection collection={collection} plants={plants} deletePlantfromCollection={deleteThePlantfromCollection}/>
+          <div style={{position: 'fixed', top: '120px', right: '20px'}}>
+            <FabButton title="Edit Collection" button="edit" handleAction={handleOpen} right="40px" />
+          </div>
+            
+        <Modal
+          open={open}
+          onClose={handleClose}
+        > 
+          <CollectionInput collection={collection}  handleSubmit={handleSubmit} /> 
+        </Modal>
+
+        <div style={{position: 'fixed', top: '190px', right: '20px'}}>
+          <FabButton title="Delete Collection" button="delete" handleAction={deleteTheCollection} right="50px" />
+        </div>
+           
+    </div>
+  )
 }
 
-const mapStateToProps = state => {
-    return {
-        collections: state.collections,
-        user: state.user,
-        plants: state.plants
-    }
-}
-
-
-export default connect(mapStateToProps, { fetchCollections, editCollection, deleteCollection, deletePlantfromCollection }) (CollectionContainer);
+export default CollectionContainer;
